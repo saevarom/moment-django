6b5ff466a1d1c35b2981be58f03184c0ceeebc3d
Sævar Öfjörð Magnússon <saevar@overcast.is>
2020-10-10 01:10:00 +0000

    Polyfill

--- moment-django.js
+++ moment-django.js
@@ -1,3 +1,15 @@
+if (!String.prototype.includes) {
+  String.prototype.includes = function(search, start) {
+    'use strict';
+
+    if (search instanceof RegExp) {
+      throw TypeError('first argument must not be a RegExp');
+    } 
+    if (start === undefined) { start = 0; }
+    return this.indexOf(search, start) !== -1;
+  };
+}
+
 (function () {
   var moment, replacements, callables;
 
